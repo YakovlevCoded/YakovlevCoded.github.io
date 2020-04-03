db0c40ac97d18a56e2651783e3a887f2cbf9bdec
Леня Яковлев <l.yakovlev@amberdata.ru>
2020-04-03 12:28:54 +0300

    water change email adress

--- mail.php
+++ mail.php
@@ -20,7 +20,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('zayavkavoda21@bk.ru'); // от кого будет уходить письмо?
-$mail->addAddress('oformlenievoda21@bk.ru');     // Кому будет уходить письмо
+$mail->addAddress('voda21rus@bk.ru');     // Кому будет уходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
